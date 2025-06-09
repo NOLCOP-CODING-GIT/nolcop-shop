e9190ef62b79e241740e238c03656a502f123e03
NOLCOP-CODING-GIT <nolcopcoding@gmail.com>
2025-06-09 03:19:14 +0200

    Réparation du build React

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Cette option permet de gérer les routes de manière propre
+  },
 })
