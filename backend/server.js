6ab7c129c0ea3311761c629b6882634391386f59
NOLCOP-CODING-GIT <nolcopcoding@gmail.com>
2025-06-09 03:26:41 +0200

    Réparation du build React

--- backend/server.js
+++ backend/server.js
@@ -102,7 +102,7 @@ app.get("/api/formulaire", (req, res) => {
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
